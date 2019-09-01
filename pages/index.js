cac3149172babf75a6d1119eca79fa24353f4093
wrightj2 <github@jonathanwright.net>
2019-09-01 14:50:51 +0100

    My first commit of test Shopify App

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>Jons Test Shopify App - It's Awesome 🎉</Heading>
+  </Page>
+);
+
+export default Index;
